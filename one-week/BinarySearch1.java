4f78f9387435b8f0db1a64944e988e809392e340
이동근 <113091695+LEEDONGKEN@users.noreply.github.com>
2024-05-09 12:55:39 +0900

    Add files via upload
    이진탐색 문제 백준2805

---
+++ one-week/BinarySearch1.java
@@ -0,0 +1,38 @@
+package arr;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch1 {
+
+    static boolean isPossible(int[] heights, int cutH, int M){
+        long sum = 0;
+        for(int h:heights)
+            if(h>cutH) sum += h - cutH;
+        return sum >= M;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] h = new int[N];
+        for (int i = 0; i < N; i++) {
+            h[i] = sc.nextInt();
+        }
+
+        Arrays.sort(h);
+
+        int l = 0, r = h[h.length-1], ans = -1;
+        while (l <= r){
+            int m = (l+r) / 2;
+            if(isPossible(h,m,M)){
+                ans = m;
+                l = m+1;
+            }
+            else r = m-1;
+        }
+
+        System.out.println(ans);
+    }
+}
