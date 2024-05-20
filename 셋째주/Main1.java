bfcdd86436948c744ad855c1e5f72c9254d0a64c
이동근 <113091695+LEEDONGKEN@users.noreply.github.com>
2024-05-20 16:35:00 +0900

    Create Main1.java

---
+++ 셋째주/Main1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] P = new int[N];
+        int totalTime = 0;
+
+        for (int i = 0; i < P.length; i++) {
+            P[i] = sc.nextInt();
+        }
+
+        Arrays.sort(P);
+
+        int add = 0; // 변수 한개가 더 필요하다.
+        for (int i = 0; i < P.length; i++) {
+            add += P[i];
+            totalTime += add;
+        }
+        System.out.println(totalTime);
+    }
+}
