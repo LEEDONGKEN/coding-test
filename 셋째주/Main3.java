d462867044f036fa1cf9188a146d6d6161324e95
이동근 <113091695+LEEDONGKEN@users.noreply.github.com>
2024-05-22 18:43:10 +0900

    Add files via upload

---
+++ 셋째주/Main3.java
@@ -0,0 +1,27 @@
+package ch4;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] l = new int[n];
+        for (int i = 0; i < l.length; i++) {
+            l[i] = sc.nextInt();
+        }
+
+        Integer[] array = Arrays.stream(l).boxed().toArray(Integer[]::new);
+        Arrays.sort(array, Collections.reverseOrder());
+
+        int[] sortedArray = Arrays.stream(array).mapToInt(Integer::intValue).toArray();
+        int gold = 0;
+
+        for (int i = 0; i < sortedArray.length-1; i++) {
+            gold += sortedArray[0] + sortedArray[i+1];
+        }
+        System.out.println(gold);
+    }
+}
