9f5ffdb2b137322a2c7931caf4644cdb36429997
이동근 <113091695+LEEDONGKEN@users.noreply.github.com>
2024-05-25 09:14:41 +0900

    Update and rename Main6.java to 30.java

--- 셋째주/30.java
+++ 셋째주/30.java
@@ -3,7 +3,7 @@ package ch4;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
-
+//백준 10610번 30
 public class Main6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
